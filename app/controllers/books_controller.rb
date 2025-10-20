df9950b8cbc9f1bd07afacdca21e00aab03ced43
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-20 16:16:21 +0900

    郵便番号・住所・自己紹介の追加、ユーザ一覧の仮作成、日本語翻訳

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
   before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books or /books.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  devise_for :users, controllers: { registratons: 'users/registrations' }
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
