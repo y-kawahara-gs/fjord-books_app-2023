fde9656baf44288591e962ae731bee5f8a8bb97e
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-21 10:21:33 +0900

    ユーザ一覧・詳細の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
   devise_for :users, controllers: { registratons: 'users/registrations' }
-  resources :books
+  resources :books, :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
