6015f6386b4147b967f342bd4bc0b4c5b32567e4
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-21 14:16:57 +0900

    ユーザー一覧に直接アクセスしたときのログイン処理の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code address profile])
   end
-
-  def after_sign_in_path_for(_resource)
-    books_path
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.order(:id).page(params[:page])
   end
