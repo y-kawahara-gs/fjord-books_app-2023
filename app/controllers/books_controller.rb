3d46a0a7bdf9842f52b347767ebe10ac50bca852
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-27 10:03:26 +0900

    コントローラーのアクセス管理を移動

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
-  before_action :authenticate_user!
-
   before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books or /books.json
