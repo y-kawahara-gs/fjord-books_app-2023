88c9174054b4e3f6e6359a76937992b4c4acb66a
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-27 12:58:21 +0900

    コメント機能の仮作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,10 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1 or /books/1.json
-  def show; end
+  def show
+    @comments = @book.comments.includes(:user).order(created_at: :desc)
+    @comment = @book.comments.build
+  end
 
   # GET /books/new
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :books do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :reports do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: %i(index show)
 end
