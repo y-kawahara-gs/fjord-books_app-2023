20c1fe74ec56fea4d79532bab28b4dc18d1846be
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-28 09:52:52 +0900

    コメント機能の実装とi18nの翻訳

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_commentable
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+      else
+        redirect_to @commentable if @comment.save
+      end
+    end
+  end
+
+  def destroy
+    @comment = @commentable.comments.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_commentable
+    if params[:report_id]
+      @commentable = Report.find(params[:report_id])
+    elsif params[:book_id]
+      @commentable = Book.find(params[:book_id])
+    end
+  end
+end
