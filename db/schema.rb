3d87bcccf1e50da239f12e3249076caba78e926d
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-28 09:54:37 +0900

    不要なマイグレーション削除とスキーマの修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_24_012431) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_24_012431) do
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
-    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -47,16 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_24_012431) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "title"
-    t.text "body"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -95,9 +84,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_24_012431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tasks", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
