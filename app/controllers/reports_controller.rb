5f69b999926c73c642c159bbfbb83eae4675484c
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-10-30 16:00:41 +0900

    言及一覧の作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,6 +9,7 @@ class ReportsController < ApplicationController
 
   def show
     @report = Report.find(params[:id])
+    @mentions = @report.mentioned_reports
   end
 
   # GET /reports/new
@@ -22,6 +23,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
+      set_mention
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -30,6 +32,8 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      @report.active_mentions.destroy_all
+      set_mention
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -50,5 +54,16 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
+  end
+
+  def set_mention
+    urls = URI.extract(@report.content, ["http"])
+    urls.map do |url|
+      next unless url.match?(/http:\/\/127.0.0.1:3000/)
+      target_id = URI.parse(url).path.split('/').last
+      next if @report.id == target_id.to_i
+      mentioned_report = Report.find(target_id)
+      @report.active_mentions.create(mentioned: mentioned_report)
+    end
   end
 end
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -2,6 +2,5 @@ class Mention < ApplicationRecord
   belongs_to :mentioner, class_name: "Report", inverse_of: :active_mentions
   belongs_to :mentioned, class_name: "Report", inverse_of: :passive_mentions
 
-  validates :mentioner_id, uniqueness: true
-  validates :mentioned_id, uniqueness: true
+  validates :mentioner_id, uniqueness: { scope: :mentioned_id }
 end
