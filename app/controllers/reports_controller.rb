849367857dd56be4710b242eecf30631d02c484b
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-11-05 10:08:33 +0900

    find_byをfindに変更、不要な処理の修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -64,8 +64,8 @@ class ReportsController < ApplicationController
       next unless url.match?(%r{http://127.0.0.1:3000/reports/})
 
       target_id = URI.parse(url).path.split('/').last
-      next if @report.id == target_id.to_i || Report.exists?(target_id) == nil
-      mentioned_report = Report.find_by(id: target_id)
+      next if @report.id == target_id.to_i
+      mentioned_report = Report.find(target_id)
       @report.active_mentions.create!(mentioned: mentioned_report)
     end
   end
