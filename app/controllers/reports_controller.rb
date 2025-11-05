862ee72074ca5a380b3729351a7f25affb65408c
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-11-05 13:50:47 +0900

    不要なロールバック処理の削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,23 +23,25 @@ class ReportsController < ApplicationController
     ActiveRecord::Base.transaction do
       @report = current_user.reports.new(report_params)
 
-      @report.save!
-      @report.create_mention!
-    rescue ActiveRecord::RecordInvalid
-      render :new, status: :unprocessable_entity
+      if @report.save
+        @report.create_mention!
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
-    redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
   end
 
   def update
     ActiveRecord::Base.transaction do
-      @report.update!(report_params)
-      @report.active_mentions.destroy_all
-      @report.create_mention!
-    rescue ActiveRecord::RecordInvalid
-      render :edit, status: :unprocessable_entity
+      if @report.update(report_params)
+        @report.active_mentions.destroy_all
+        @report.create_mention!
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
-    redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
   end
 
   def destroy
