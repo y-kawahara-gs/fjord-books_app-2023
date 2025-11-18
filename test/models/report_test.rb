0218baacf4fa8cc18573f737c760d934b17d8210
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-11-18 13:13:36 +0900

    タイムスタンプ変換created_onのテストの修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -18,7 +18,7 @@ class ReportTest < ActiveSupport::TestCase
 
   test '#created_on' do
     test_report = @test_report
-    test_report.created_at =  [Thu, 06 Nov 2025 10:25:09.180219000 JST +09:00]
-    assert_equal "a", test_report.created_on
+    test_report.created_at = "Thu, 18 Nov 2025 11:12:04.733175000 JST +09:00"
+    assert_equal "2025-11-18", "#{test_report.created_on}"
   end
 end
